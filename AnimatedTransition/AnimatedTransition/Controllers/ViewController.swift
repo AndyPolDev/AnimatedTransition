bba45b83b56d9ccb5c1228315200de77ee10bd01
AndyPolDev <AndyPolDev@gmail.com>
2023-01-12 20:18:27 +0800

    AnimatedTransition - 002. SwiftLint installed

--- AnimatedTransition/AnimatedTransition/Controllers/ViewController.swift
+++ AnimatedTransition/AnimatedTransition/Controllers/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
+
     private lazy var presentButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .red
@@ -13,18 +13,18 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         presentButton.layer.cornerRadius = presentButton.frame.width / 2
     }
-    
+
     private func setupViews() {
         title = "FirstViewController"
         view.backgroundColor = .white
     }
-    
+
     @objc private func presentButtonPressed() {
         print("presentButtonPressed")
     }
